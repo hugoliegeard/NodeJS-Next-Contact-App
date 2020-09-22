5bed44fdd08cfdb5f7816a6935308d27065be499
Hugo LIEGEARD <hugo@biyn.media>
2020-09-22 09:56:09 +0200

    Travail du 22/09
    Découverte du Framework Express

---
+++ server-express.js
@@ -0,0 +1,58 @@
+/**
+ * Importe le Framework Express
+ */
+const express = require('express');
+
+/**
+ * Initialisation de notre Application Express
+ * @type {*|Express}
+ */
+const app = express();
+const port = 3000;
+
+/**
+ * Configuration du Templating avec Handlebar
+ */
+const hbs = require('express-handlebars');
+app.engine('hbs', hbs({
+    extname: 'hbs',
+    defaultLayout: 'layout',
+    layoutsDir: __dirname + '/views/layouts/',
+    partialsDir: __dirname + '/views/partials/',
+}));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', hbs);
+
+/**
+ * Permet de gérer l'affichage de nos assets
+ * https://expressjs.com/fr/starter/static-files.html
+ */
+app.use('/public',
+    express.static(__dirname + '/public'));
+
+/** Page Accueil */
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello World !</h1>');
+    // res.sendFile(__dirname + '/views/html/index.html');
+    
+});
+
+/** Page Contacts */
+app.get('/contacts', (req, res) => {
+    res.sendFile(__dirname + '/views/html/contacts.html');
+});
+
+/** Gestion des erreurs 404 **/
+app.use(function(req, res, next) {
+    res.status(404).sendFile(__dirname + '/views/html/error.html')
+});
+
+/**
+ * Démarrage du serveur et écoute
+ * des connexions sur le port 3000
+ */
+app.listen(port, ()=> {
+    console.log(`Server is running at http://localhost:${port}/`);
+    console.log(`Press CTRL + C to stop\n`);
+});
